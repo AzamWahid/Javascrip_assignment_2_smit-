339b51da940c9672fe3c99022ed1bf683cf8877f
Muhammad Azam <AzamWahid@outlook.com>
2023-04-10 17:32:52 +0500

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function searchInArray(myArray, searchVal, searchIndex = 0) {
     }
 }
 
-let checkArray = [1,2,5,6,7];
+let checkArray = [1, 2, 5, 6, 7];
 console.log(searchInArray(checkArray, 8));
 
 /* 3. Write a function that adds a new paragraph element to the bottom of an
@@ -68,22 +68,20 @@ The function should take two arguments: the first argument is a reference
 to the HTML element, and the second argument is a string representing
 the new background color */
 
-function changBgColour(elementRef, BgColor)
-{
+function changBgColour(elementRef, BgColor) {
     document.getElementById(elementRef).style.backgroundColor = BgColor;
 }
-changBgColour("1p","red");
-changBgColour("2p","green");
-changBgColour("3p","blue");
+changBgColour("1p", "red");
+changBgColour("2p", "green");
+changBgColour("3p", "blue");
 
 /* 6. Write a function that saves an object to localStorage. The function should
 take two arguments: the first argument is a string representing the key to
 use for storing the object, and the second argument is the object to store.*/
 
-function saveLocalstorage(storageKey, storageObject)
-{
+function saveLocalstorage(storageKey, storageObject) {
     let stringfy = JSON.stringify(storageObject);
-    localStorage.setItem(storageKey,stringfy);
+    localStorage.setItem(storageKey, stringfy);
 }
 
 let newObject = {
@@ -96,15 +94,14 @@ let newObject1 = {
     "fname": "anwer",
     "rNo": "123",
 }
-saveLocalstorage("Intro",newObject)
-saveLocalstorage("FIntro",newObject1)
+saveLocalstorage("Intro", newObject)
+saveLocalstorage("FIntro", newObject1)
 
 /*7. Write a function that retrieves an object from localStorage. The function
 should take one argument, which is a string representing the key used to
 store the object. The function should return the object.*/
 
-function getLocalstorage(storageKey)
-{
+function getLocalstorage(storageKey) {
     let prevStorage = localStorage.getItem(storageKey);
     let getItems = prevStorage ? JSON.parse(localStorage.getItem(storageKey)) : [];
     return getItems;
@@ -121,3 +118,24 @@ console.log(wrong);
 localStorage using the property name as the key and the property value as
 the value. The function should also retrieve the object from localStorage
 and return it as a new object */
+
+function objectStorage(obj) {
+    Object.keys(obj).forEach(function (key, index) {
+        localStorage.setItem(key, JSON.stringify(obj[key]));
+    });
+
+    let saveObject = {};
+    Object.keys(obj).forEach(function (key, index) {
+        let value =  localStorage.getItem(key);
+        saveObject[key] = JSON.parse(value);
+    })
+    return saveObject;
+}
+
+let makeObject = {
+    "name": "Muhammad azam",
+    "fname": "abdul wahid",
+    "course": "javascript"
+}
+let getobject = objectStorage(makeObject);
+console.log(getobject);
